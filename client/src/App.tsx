25b625b0f21273f80ecb08e712959fbd24583eaa
Kasper <kasper@summadigita.com>
2023-01-15 16:35:38 +0100

    Change to tickle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Tickle <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
